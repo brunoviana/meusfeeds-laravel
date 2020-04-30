0fcedd946dba88e494f7901b9da299ec35b8a384
Bruno Viana <brunoviana@gmail.com>
2020-04-30 13:35:06 -0300

    Ajustes na busca de artigo

--- framework/app/Services/BuscadorDeArtigos.php
+++ framework/app/Services/BuscadorDeArtigos.php
@@ -31,17 +31,20 @@ class BuscadorDeArtigos implements BuscadorDeArtigosInterface
 
         foreach ($result->getFeed() as $item) {
             $autor = $item->getAuthor() ? (string) $item->getAuthor()->getName() : '';
+            $descricao = $this->limpaDescricao($item->getDescription());
 
             $artigosEncontrados[] = Artigo::novo(
                 $item->getTitle(),
-                $item->getDescription(),
+                $descricao,
                 $item->getLink(),
                 new Autor($autor),
                 new Data(
                     $item->getLastModified()->format('Y'),
                     $item->getLastModified()->format('m'),
                     $item->getLastModified()->format('d'),
-                )
+                ),
+                $feed->id(),
+                Artigo::NAO_LIDO
             );
         }
 
@@ -68,44 +71,50 @@ class BuscadorDeArtigos implements BuscadorDeArtigosInterface
         $artigosEncontrados = [];
 
         foreach ($result->getFeed() as $item) {
+            $artigo = ArtigoModel::where('link', $item->getLink())
+                                    ->where('feed_id', $feed->id())
+                                    ->get()
+                                    ->first();
+
+            if($artigo){
+                continue;
+            }
+
             $autor = $item->getAuthor() ? (string) $item->getAuthor()->getName() : '';
+            $descricao = $this->limpaDescricao($item->getDescription());
 
             $artigosEncontrados[] = Artigo::novo(
                 $item->getTitle(),
-                $item->getDescription(),
+                $descricao,
                 $item->getLink(),
                 new Autor($autor),
                 new Data(
                     $item->getLastModified()->format('Y'),
                     $item->getLastModified()->format('m'),
                     $item->getLastModified()->format('d'),
-                )
+                ),
+                $feed->id(),
+                Artigo::NAO_LIDO
             );
         }
 
         return $artigosEncontrados;
     }
 
-    // public function buscar(string $link, string $aPartirDe = '') : array
-    // {
-    //     if (!empty($aPartirDe)) {
-    //         $result = $this->feedIo->readSince($link, (new \DateTime($aPartirDe))->modify('-1 day'));
-    //     } else {
-    //         $result = $this->feedIo->read($link);
-    //     }
+    protected function limpaDescricao($descricao)
+    {
+        if (!$descricao) {
+            return '';
+        }
 
-    //     $artigosEncontrados = [];
+        $semHtml = strip_tags($descricao);
 
-    //     foreach ($result->getFeed() as $item) {
-    //         $artigosEncontrados[] = [
-    //             'titulo' => $item->getTitle(),
-    //             'descricao' => $item->getDescription(),
-    //             'link' => $item->getLink(),
-    //             'autor' => $item->getAuthor() ? (string) $item->getAuthor()->getName() : '',
-    //             'data_publicacao' => $item->getLastModified()->format('Y-m-d'),
-    //         ];
-    //     }
+        preg_match('#.*[\.\?!]#', $semHtml, $limitado);
 
-    //     return $artigosEncontrados;
-    // }
+        if (isset($limitado[0])) {
+            return trim($limitado[0]);
+        }
+
+        return trim($semHtml);
+    }
 }
