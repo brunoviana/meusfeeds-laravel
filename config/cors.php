612e6c2e4573c217a49bbfa70f924dbfcd7dbb28
Bruno Viana <brunoviana@gmail.com>
2020-05-23 09:47:46 -0300

    arrumando origin

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'meusfeeds.com.br'
+    ],
 
     'allowed_origins_patterns' => [],
 
